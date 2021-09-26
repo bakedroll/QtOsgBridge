9df49f8e370738bba4221cbb4480375e735e49f7
Daniel Borchart <daniel.borchart@googlemail.com>
2021-09-26 15:30:21 +0200

    removed macros header

--- QtOsgBridge/include/QtOsgBridge/Macros.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <utilsLib/Utils.h>
-
-#define UTILS_QLOG_DEBUG(msg) UTILS_LOG_DEBUG(std::string(msg.toLocal8Bit().constData()))
-#define UTILS_QLOG_INFO(msg) UTILS_LOG_INFO(std::string(msg.toLocal8Bit().constData()))
-#define UTILS_QLOG_WARN(msg) UTILS_LOG_WARN(std::string(msg.toLocal8Bit().constData()))
-#define UTILS_QLOG_FATAL(msg) UTILS_LOG_FATAL(std::string(msg.toLocal8Bit().constData()))
